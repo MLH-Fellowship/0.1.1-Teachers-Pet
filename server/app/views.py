a28c9c86e47b3eae8ab4c8ebb968928bea089a6c
korusuke <karan.sheth@somaiya.edu>
2020-06-04 18:04:49 +0530

    Add upload route

--- server/app/views.py
+++ server/app/views.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-
+import os
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'media' not in request.files:
+        return 'Media not found'
+    media = request.files['media']
+    media.save(os.path.join('./uploads', media.filename))
+    return 'ok'
