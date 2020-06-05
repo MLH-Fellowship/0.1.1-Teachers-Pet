0c241c087593d951a7ca21473033dd0e41a2fb6e
korusuke <karan.sheth@somaiya.edu>
2020-06-05 18:21:54 +0530

    Add api endpoints

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Load the views
 from app import views
